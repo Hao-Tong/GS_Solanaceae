f29486a3e63ce348031d2bd890dbe9a6b1f10423
Hao Tong <52068692+Hao-Tong@users.noreply.github.com>
2021-10-19 10:36:25 +0200

    Add files via upload

---
+++ tomato/RF_CD/RF_caret.R
@@ -0,0 +1,274 @@
+#!/usr/bin/Rscript
+  
+# Genome Selection model RF
+# All models are implemented with R package caret
+# Prediction accuracy and coincidence from cross-validations
+# Contact: tong@mpimp-golm.mpg.de
+
+##########################################################################
+## add path and packages
+dir <- "D:/04_tomatoperpper/GS/tomato/"
+setwd(dir)
+
+library(caret)
+library(e1071)
+library(randomForest)
+#library(kernlab)
+#library(RSNNS)
+#library(fastDummies)
+library(pROC)
+library(UBL)
+options(digits = 15)
+
+##########################################################################
+## load data
+# only modern accessions
+modern <- read.table("data/modern.txt",header=F,sep="\t")[,1]
+
+# phenotypic data
+myY_CD1 <- read.table("data/Tomato.AG.csv",head=TRUE,sep=",")
+myY_CD2 <- read.table("data/Tomato.AG.add.csv",head=TRUE,sep=",")
+myY_CD <- rbind(myY_CD1,myY_CD2)
+rownames(myY_CD) <- myY_CD[,1]
+myY_CD <- myY_CD[,-1]
+myY_CD <- myY_CD[modern,]
+
+# selected CD traits
+#match_CD <- read.table("data/tomato_match_TA_CP_final.csv",head=FALSE,sep=",")
+#match_CD <- c(6,7,9,13,26)
+match_CD <- c(13,26)
+classpheno <- myY_CD[,match_CD]
+
+# if more than one classes in one cell, then assign as NA 
+# (i.e. the number must be integer)
+for (i in 1:ncol(classpheno)){
+  naid <- which(classpheno[,i]%%1!=0)
+  classpheno[naid,i] <- NA
+}
+
+# genotypic data
+myG <- read.table("data/tomato_snp_maf5_filter_num.txt",head=TRUE,sep="\t")
+geno <- myG[,-1]
+rownames(geno) <- myG[,1]
+geno <- as.matrix(geno[modern,])
+
+# cross-validation fold id
+idsall <- read.table("fold/foldid.csv",sep=",",header=F)
+rr <- 30 #replicate number
+f <- 5 #fold number
+
+##########################################################################
+##########################################################################
+### function random forest model
+RF <- function(xx,name){
+  
+  for (n in 1:ncol(classpheno)){
+    
+    print(paste("Phenotype ",n," Start!",sep=""))
+    Y <- classpheno[,n]
+    Y[which(Y != "NA")] <- paste0("s",Y[which(Y != "NA")])
+    nle <- nlevels(as.factor(Y))
+    
+    ypred_all <- matrix(NA,nrow(classpheno),rr)
+    ypredp_all <- matrix(NA,nrow(classpheno),rr*nle)
+    
+    for (r in 1:rr){
+      
+      ids <- idsall[,r]
+      
+      for (p in 1:f){
+        
+        print(paste("Phenotype ",n," Replicate ",r,".",p," Start!",sep=""))
+        
+        trset <- which(ids!=p)
+        teset <- which(ids==p)
+        xxtr <- xx[trset,]
+        xxte <- xx[teset,]
+        ytr <- as.factor(Y[trset])
+        
+        # remove the NA data accessions
+        naid <- which(as.numeric(is.na(ytr))==1)
+        if (length(naid)!=0){
+          xxtr <- xxtr[-naid,]
+          ytr <- ytr[-naid]
+        }
+        
+        trdata <- data.frame(ytr,xxtr)
+        #table(trdata$ytr)
+        #newtrdata <- smote(ytr~., trdata, perc.over=20, perc.under=20)
+        newtrdata <- SmoteClassif(ytr~., trdata, k=1, C.perc="balance")
+        #table(newtrdata$ytr)
+        
+        #folds <- createMultiFolds(ytr,k=5,times=1)
+        tcontr <- trainControl(method="repeatedcv",number=5,repeats=1,classProbs=TRUE)
+        rffit <- caret::train(ytr~., data=newtrdata, method="rf", metric="Accuracy",
+            na.action=na.pass, trControl=tcontr, tuneGrid=expand.grid(.mtry=sqrt(ncol(xxtr))),ntree=1000)
+        
+        #ytr <- newtrdata[,1]
+        #xxtr <- newtrdata[,-1]
+        #rffit1 <- caret::train(x=xxtr, y=ytr, method="rf", metric="Accuracy",
+  			#		trControl=tcontr, tuneGrid=expand.grid(.mtry=sqrt(ncol(xxtr))),ntree=1000)
+        
+        ypredp <- predict(rffit,newdata=xxte,type="prob")
+        ypred <- predict(rffit,newdata=xxte)
+        
+        ypred_all[teset,r] <- as.character(ypred)
+        
+        if (ncol(ypredp)==nle){
+          ypredp_all[teset,c((nle*(r-1)+1):(nle*r))] <- as.matrix(ypredp)
+          #colnames(ypredp_all)[c((nle*(r-1)+1):(nle*r))] <- colnames(ypredp)
+        } else {
+          # in the case less predicted class than the raw data 
+          mid <- levels(as.factor(Y)) %in% colnames(ypredp)
+          mid <- levels(as.factor(Y))[which(as.numeric(mid)==0)]
+          ypredpp <- cbind(as.matrix(ypredp),rep(0,length(teset)))
+          colnames(ypredpp) <- c(colnames(ypredp),mid) 
+          ypredp_all[teset,c((nle*(r-1)+1):(nle*r))] <- ypredpp[,levels(as.factor(Y))]
+          #colnames(ypredp_all) <- c(colnames(ypredp),mid)       
+        }
+        
+      }
+    }
+    
+    write.table(ypred_all,paste("RF_CD/predict_",name,"_trait_",n,".csv",sep=""),sep=",",row.names=F,col.names=F)
+    write.table(ypredp_all,paste("RF_CD/predict_probability_",name,"_trait_",n,".csv",sep=""),sep=",",row.names=F,col.names=F)
+    
+  }
+}
+
+### function predictability of classification models
+ability_classification <- function(name){
+  
+  accall <- matrix(NA,rr*f,ncol(classpheno))
+  aucall <- matrix(NA,rr*f,ncol(classpheno))
+  match1all <- matrix(NA,rr*f,ncol(classpheno))
+  match2all <- matrix(NA,rr*f,ncol(classpheno))
+  
+  for (n in 1:ncol(classpheno)){
+    
+    print(paste("Phenotype ",n," Start!",sep=""))
+    Ypred <- read.table(paste("RF_CD/predict_",name,"_trait_",n,".csv",sep=""),head=F,sep=",")
+    Ypredp <- read.table(paste("RF_CD/predict_probability_",name,"_trait_",n,".csv",sep=""),head=F,sep=",")
+    Y <- classpheno[,n]
+    Y[which(Y != "NA")] <- paste0("s",Y[which(Y != "NA")])
+    nle <- nlevels(as.factor(Y))
+    
+    for (r in 1:rr){
+      
+      ids <- idsall[,r]
+      accp <- NULL
+      aucp <- NULL
+      match1p <- NULL
+      match2p <- NULL
+      
+      for (p in 1:f){
+        teset <- which(ids==p)
+        yte <- as.factor(Y[teset])
+        ypred <- as.factor(Ypred[teset,r])
+        ypredp <- Ypredp[teset,c((nle*(r-1)+1):(nle*r))]
+        colnames(ypredp) <- levels(as.factor(Y))
+        
+        naid <- which(as.numeric(is.na(yte))==1)
+        if (length(naid)!=0){
+          ypred <- ypred[-naid]
+          ypredp <- ypredp[-naid,]
+          yte <- yte[-naid]
+        }
+        
+        if (nlevels(yte)>=2){
+          ## auc
+          predauc <- multiclass.roc(yte,ypredp)
+          aucp <- rbind(aucp,as.numeric(predauc$auc))
+          
+          ## accuracy
+          predacc <- defaultSummary(data.frame("obs"=yte,"pred"=ypred))
+          accp <- rbind(accp,predacc[1])
+          #kappap <- rbind(kappap,predacc[2])
+          
+          # coincidence in top one class
+          ytetop <- which(yte==levels(as.factor(Y))[nle])
+          ypredtop <- which(ypred==levels(as.factor(Y))[nle])
+          if (length(ytetop) != 0){
+            match1 <- sum(ytetop %in% ypredtop)/length(ytetop)
+          } else {
+            match1 <- NA
+          }
+          match1p <- rbind(match1p,match1)
+          
+          # coincidence in top two classes
+          ytetop <- which(yte==levels(as.factor(Y))[nle-1])
+          ypredtop <- which(ypred==levels(as.factor(Y))[nle-1])
+          if (length(ytetop) != 0){
+            match2 <- sum(ytetop %in% ypredtop)/length(ytetop)
+          } else {
+            match2 <- NA
+          }
+          match12 <- mean(c(match1,match2),na.rm=T)
+          match2p <- rbind(match2p,match12)
+        } else {
+          aucp <- rbind(aucp,NA)
+          accp <- rbind(accp,NA)
+          match1p <- rbind(match1p,NA)
+          match2p <- rbind(match2p,NA)
+        }
+        
+      }
+      accall[(f*r-(f-1)):(f*r),n] <- accp
+      aucall[(f*r-(f-1)):(f*r),n] <- aucp
+      match1all[(f*r-(f-1)):(f*r),n] <- match1p
+      match2all[(f*r-(f-1)):(f*r),n] <- match2p
+    }
+    
+  }
+  
+  accallm <- rbind(accall,colMeans(accall,na.rm=T))
+  aucallm <- rbind(aucall,colMeans(aucall,na.rm=T))
+  match1allm <- rbind(match1all,colMeans(match1all,na.rm=T))
+  match2allm <- rbind(match2all,colMeans(match2all,na.rm=T))
+  
+  # Output: prediction accuracy and coincidence
+  write.table(aucallm,paste("RF_CD/auc_",name,".csv",sep=""),sep=",",row.names=F,col.names=F)
+  write.table(accallm,paste("RF_CD/accuracy_",name,".csv",sep=""),sep=",",row.names=F,col.names=F)
+  write.table(match1allm,paste("RF_CD/coincidence1_",name,".csv",sep=""),sep=",",row.names=F,col.names=F)
+  write.table(match2allm,paste("RF_CD/coincidence2_",name,".csv",sep=""),sep=",",row.names=F,col.names=F)
+  
+  results <- rbind(colMeans(aucall,na.rm=T),colMeans(accall,na.rm=T),colMeans(match1all,na.rm=T),colMeans(match2all,na.rm=T),
+                   apply(aucall,2,sd,na.rm=T),apply(accall,2,sd,na.rm=T),apply(match1all,2,sd,na.rm=T),apply(match2all,2,sd,na.rm=T))
+  results <- t(results)
+  results <- cbind(colnames(pheno),results)
+  colnames(results) <- c("Trait","Mean-AUC","Accurancy","Top1","Top2","SD-AUC","Accurancy","Top1","Top2")
+  write.table(results,paste("RF_CD/summary_",name,".csv",sep=""),sep=",",row.names=F,col.names=T)
+  
+}
+
+##########################################################################
+### test for different tag SNP datasets 
+
+p <- "10"
+tagname_all <- NULL
+for (i in 1:12){
+  allname <- read.table(paste0("tag/chr",i,".100.SNPid.txt"),head=F,sep="\t")[,1]
+  tagid <- read.table(paste0("tag/chr",i,".",p,".tagSNP.txt"),head=F,sep="\t")[,1]
+  #ntag <- length(tagid)
+  #ntag_all <- c(ntag_all,ntag)
+  tagid <- tagid+1
+  tagname <- allname[tagid]
+  tagname_all <- c(tagname_all,tagname)
+}
+
+# filter tag snps
+tagname_all <- gsub("-",".",tagname_all)
+tagname_all <- paste0("X",tagname_all)
+genotag <- geno[,tagname_all]
+
+# run the GS model
+RF(as.matrix(genotag),paste0("tagsnp.",p))
+ability_classification(paste0("tagsnp.",p))
+
+##########################################################################
+# recalculate predictability
+p <- "10"
+ability_classification(paste0("tagsnp.",p))
+
+##########################################################################
+
---
+++ tomato/rrBLUP/00_t-test_tomato.vs.pepper.R
@@ -0,0 +1,60 @@
+# t-test for significant difference
+# Contact: tong@mpimp-golm.mpg.de
+
+##########################################################################
+## add path and packages
+dir <- "D:/04_tomatoperpper/GS/"
+setwd(dir)
+
+##########################################################################
+# tomato traits
+todata <- read.table("tomato/rrBLUP/pearson_allsnp.csv",sep=",",header=F)
+
+# pepper trait 
+pedata <- read.table("pepper/rrBLUP/pearson_allsnp.csv",sep=",",header=F)
+
+# t test
+# each traits
+pvalall <- NULL
+for (i in 1:47){
+  togs <- todata[c(1:150),i]
+  pegs <- pedata[c(1:150),i]
+  test <- t.test(togs,pegs,alternative="two.sided",na.action=na.omit,paired=TRUE)
+  pval <- test$p.value
+  pvalall <- rbind(pvalall,pval)
+}
+pvalall <- cbind(c(1:47),pvalall)
+write.table(pvalall,"tomato/rrBLUP/ttest_trait.csv",sep=",",row.names=F,col.names=F)
+
+##########################################################################
+# t test
+# each category
+category <- read.table("tomato/data/trait_group.csv",head=TRUE,sep=",")
+
+group <- category[,2]
+group_u <-  unique(group)
+
+pvalall <- NULL
+for (j in 1:length(group_u)){
+  ids <- which(group==group_u[j])
+  togs <- todata[1:150,ids]
+  pegs <- pedata[1:150,ids]
+  togsc <- c(as.matrix(togs))
+  pegsc <- c(as.matrix(pegs))
+  test <- t.test(togsc,pegsc,alternative="two.sided",na.action=na.omit,paired=TRUE)
+  pval <- test$p.value
+  pvalall <- rbind(pvalall,pval)
+}
+
+togsall <- todata[1:150,1:47]
+pegsall <- pedata[1:150,1:47]
+togsallc <- c(as.matrix(togsall))
+pegsallc <- c(as.matrix(pegsall))
+test <- t.test(togsallc,pegsallc,alternative="two.sided",na.action=na.omit,paired=TRUE)
+pval <- test$p.value
+pvalall <- rbind(pvalall,pval)
+
+pvalall <- cbind(c(1:11),pvalall)
+write.table(pvalall,"tomato/rrBLUP/ttest_group.csv",sep=",",row.names=F,col.names=F)
+
+##########################################################################
